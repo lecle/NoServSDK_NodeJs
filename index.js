4be5ca0610f646bc7b2324b6962e0778b9d76ded
daesun <yume@yumes.net>
2015-08-19 18:50:53 +0900

    add the request queue

--- index.js
+++ index.js
@@ -4,7 +4,24 @@ var request = require('request');
 
 module.exports = require('./javascriptSDK/noserv.js');
 
+module.exports.isRunning = false;
+module.exports.reqQueue = [];
+
 module.exports.Noserv.sendAjax = function(method, url, data, callF, addF){
+
+    if(module.exports.isRunning) {
+        module.exports.reqQueue.push({
+            method : method,
+            url : url,
+            data : data,
+            callF : callF,
+            addF : addF
+        });
+
+        return;
+    }
+
+    module.exports.isRunning = true;
 
     var options = {
         method : method,
@@ -19,24 +36,33 @@ module.exports.Noserv.sendAjax = function(method, url, data, callF, addF){
     request(options, function(err, res) {
 
         if(err) {
-
             if (callF.error)
-                return callF.error(data, err.message);
+                callF.error(data, err.message);
+        } else {
 
-            return;
+            var response = null;
+
+            if(res.body && res.body.charAt && res.body.charAt(0) === '{')
+                response = JSON.parse(res.body);
+            else
+                response = res.body;
+
+            if(addF)
+                addF(response);
+
+            if(callF && callF.success)
+                callF.success(response);
         }
 
-        var response = null;
+        module.exports.isRunning = false;
 
-        if(res.body && res.body.charAt && res.body.charAt(0) === '{')
-            response = JSON.parse(res.body);
-        else
-            response = res.body;
+        if(module.exports.reqQueue.length > 0) {
 
-        if(addF)
-            addF(response);
+            var arg = module.exports.reqQueue.shift();
+            process.nextTick(function() {
 
-        if(callF && callF.success)
-            callF.success(response);
+                module.exports.Noserv.sendAjax(arg.method, arg.url, arg.data, arg.callF, arg.addF);
+            });
+        }
     });
 };
